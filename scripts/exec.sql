a6fc7cf5cd1317024984c959dad77a5545899642
moririn2528 <strangenoise1@gmail.com>
2023-11-25 15:33:35 +0900

    add index

--- scripts/exec.sql
+++ scripts/exec.sql
@@ -13,5 +13,7 @@ RENAME USER 'isudns'@'localhost' to 'isudns'@'%';
 /* add index for SELECT * FROM livestream_tags WHERE livestream_id = N */
 -- ALTER TABLE `livestream_tags` ADD INDEX `livestream_id` (`livestream_id`);
 
-ALTER TABLE icons ADD icon_path VARCHAR(255) NOT NULL DEFAULT '../img/NoImage.jpg';
+-- ALTER TABLE icons ADD icon_path VARCHAR(255) NOT NULL DEFAULT '../img/NoImage.jpg';
 -- ALTER TABLE icons DROP icon_path;
+
+ALTER TABLE `ng_words` ADD INDEX `ng_words_user_livestream_index` (`user_id`,`livestream_id`);
