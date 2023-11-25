1076f006127ec6cbf087f540031b2afd39bda099
Saza-ku <the.latticeheart@gmail.com>
2023-11-25 15:58:39 +0900

    wait db

--- webapp/pdns/init_zone.sh
+++ webapp/pdns/init_zone.sh
@@ -13,4 +13,3 @@ temp_dir=$(mktemp -d)
 trap 'rm -rf $temp_dir' EXIT
 sed 's/<ISUCON_SUBDOMAIN_ADDRESS>/'$ISUCON_SUBDOMAIN_ADDRESS'/g' u.isucon.dev.zone > ${temp_dir}/u.isucon.dev.zone
 pdnsutil load-zone u.isucon.dev ${temp_dir}/u.isucon.dev.zone
-
