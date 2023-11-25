1105defe0b59c08aac7f7e3d9f3d0bd7bb0bd54e
Hiroya Onoe <git@onoe.dev>
2023-11-25 11:14:31 +0900

    index comment

--- scripts/exec.sql
+++ scripts/exec.sql
@@ -1,1 +1,3 @@
 SELECT table_name, engine, table_rows, avg_row_length, floor((data_length+index_length)/1024/1024) as allMB, floor((data_length)/1024/1024) as dMB, floor((index_length)/1024/1024) as iMB FROM information_schema.tables WHERE table_schema=database() ORDER BY (data_length+index_length) DESC;
+
+ALTER TABLE `livecomments` ADD INDEX `livestream_id_created_idx` (`livestream_id`, `created_at`);
